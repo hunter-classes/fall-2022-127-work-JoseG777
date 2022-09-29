e3c713723686609ca024effd2465db087a7831eb
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2022-09-29 02:29:03 +0000

    added code for finished piglatin assignment

--- piglatin/piglatin.py
+++ piglatin/piglatin.py
@@ -1,17 +1,49 @@
 def piglatin(word):
     first = word[0:1].lower()
-    if first == "a" or first == "e" or first == "i" or first == "o" or first == "u" or first == "y":
-        return word.capitalize() + "yay"
-    else:
-        new = word.replace(word[0:1], "") + word[0:1] + "ay"
+    if first[0] in "aeiou":
+        if "." in word:
+            x = word.replace(".", "")
+            return x.capitalize() + "yay" + "."
+        elif "!" in word:
+            x = word.replace("!", "")
+            return x.capitalize() + "yay" + "!"
+        elif "?" in word:
+            x = word.replace("?", "")
+            return x.capitalize() + "yay" + "?"
+        else:
+            return word.capitalize() + "yay"
+    if first[0] in "bcdfghjklmnpqrstvwxyz":
+        if "." in word:
+            x1 = word[0:1]
+            x2 = word.replace(word[0:1], "")
+            x = x2.replace(".", "") + word[0:1] + "ay" + "."
+            return x.capitalize()
+        elif "!" in word:
+            x1 = word[0:1]
+            x2 = word.replace(word[0:1], "")
+            x = x2.replace("!", "") + word[0:1] + "ay" + "!"
+            return x.capitalize()
+        elif "?" in word:
+            x1 = word[0:1]
+            x2 = word.replace(word[0:1], "")
+            x = x2.replace("?", "") + word[0:1] + "ay" + "?"
+            return x.capitalize()
+        else:
+            new = word.replace(word[0:1], "") + word[0:1] + "ay"
         return new.capitalize()
-        
+      
     
+
         
-print(piglatin("apple"))
-print(piglatin("bike"))
-print(piglatin("your"))
-print(piglatin("hello"))
+
+print(piglatin("Apple."))
+print(piglatin("Apple!"))
+print(piglatin("Apple?"))
+print(piglatin("BIKE."))
+print(piglatin("BIKE!"))
+print(piglatin("BIKE?"))
+print(piglatin("BIKE"))
+
 
 #Bond
 def bondify(name):
