4b45e250cb24926e4df86359ac239f11433e5e0c
Your Name <JoseG777>
2022-10-25 23:42:56 -0400

    finished code for modes.py

---
+++ mode/modes.py
@@ -0,0 +1,17 @@
+list = [43, 42, 44, 43, 43, 56, 54, 55, 1, 11, 11, 43]
+def findLargest(list):
+    largest = list[0]
+    for i in list:
+        if i > largest:
+            largest = i
+    return largest
+print(findLargest(list))
+
+def freq(list):
+    num = int(input("What number would you like to find?"))
+    total = 0
+    for i in list:
+        if num == i:
+            total = total + 1
+    return total
+print (freq(list))
