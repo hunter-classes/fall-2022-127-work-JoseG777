c9257e53e718abd68a8f653c45aa4974004c0ae0
Your Name <JoseG777>
2022-10-27 09:49:11 -0400

    in class work modeCW.py

---
+++ mode/modeCW.py
@@ -0,0 +1,17 @@
+from timeit import repeat
+
+list = [44, 44, 44, 44, 44, 32, 32, 33, 34, 56, 76, 766, 44]
+def mode(list):
+    first = 0
+    start = list[first]
+    largest = list.count(start)
+    biggest = list[first]
+    for i in list:
+        if largest < list.count(list[first + 1]):
+            newlargest = list.count(list[first + 1])
+            largest = newlargest
+            biggest = i
+    return biggest
+print(mode(list))
+
+
