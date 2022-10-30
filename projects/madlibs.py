8c7504146ecd7505ae51cd213d145eb52b749862
JoseG777 <104945899+JoseG777@users.noreply.github.com>
2022-10-30 02:42:22 +0000

    update

--- projects/madlibs.py
+++ projects/madlibs.py
@@ -1,3 +1,27 @@
+import random
+
+VERBS = ["DESTROY", "FIGHT", "WALK", "RUN"]
+NOUN = ["PUPPY", "DRAGON", "APPLE", "ORANGE"]
+HERO = ["Jose", "Ace", "Zero"]
+
 s1 = open('story.dat')
 s2 = s1.read()
-print(s2)
+
+def dh(H):
+  H1 = random.choice(H)
+  s3 = s2.replace("<HERO>", H1)
+  return s3
+
+s4 = dh(HERO)  
+
+
+def dv(V):
+  s5 = s4.split()
+  for i in s5:
+    if i == '<VERB>':
+      i = random.choice(V)
+  return s5
+  
+  
+print(dv(VERBS)) 
+
