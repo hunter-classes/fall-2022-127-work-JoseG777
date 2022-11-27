83243da54528339c3a83e736629509ce38e082f1
Your Name <JoseG777>
2022-11-27 14:19:18 -0500

    project 2

--- project-02-speak/speak.py
+++ project-02-speak/speak.py
@@ -1,9 +1,17 @@
-s1 = open('input.txt') 
-s2 = s1.read()
+text = open('input.txt')
+text1 = text.read()
+text2 = text1.lower()
+text3 = text2.split()
 
-s3 = open('pirates.dat')
-s4 = s3.read()
-s5 = s4.split()
+dict = {'of': "o'", 'buddy':'matey', 'before':'afore', 'guys':'scurvey dogs', 'hero':'pirate', "and":"an'"}
 
-print(s2)
-print(s5)
+text4 =[]
+for word in text3:
+    if word in dict.keys():
+        text4.append(dict[word])
+    else:
+        text4.append(word)
+
+print(" ".join(text4))
+    
+
